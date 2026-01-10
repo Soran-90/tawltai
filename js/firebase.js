3751e74831e85939e6ddca79bc03be1948efb218
Soran-90 <158614998+Soran-90@users.noreply.github.com>
2026-01-10 23:34:27 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: ضع إعدادات مشروعك من Firebase Console
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDgMmhFIjnLWl4cclM2V5-M4EvaPG-QtfE",
   authDomain: "tawltai.firebaseapp.com",
@@ -11,5 +13,5 @@ const firebaseConfig = {
   appId: "1:232076374782:web:df11501e8a73b667d72baf"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
