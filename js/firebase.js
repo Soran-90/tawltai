1ef9ba0aa88aa41161ee392fd23a3412a9cf5e9c
Soran-90 <158614998+Soran-90@users.noreply.github.com>
2026-01-11 18:51:28 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDgMmhFIjnLWl4cclM2V5-M4EvaPG-QtfE",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
